4d5dc47d69b84215dca49e8dbcdb0caf64476d6c
akadashiso <akadashiso@gmail.com>
2021-05-15 17:46:38 +0900

    [Finish]Section17-238 : baseURLやheaderなど、全てのリクエストに同じ設定をつける方法を学ぶ

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,67 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app">
+    <h3>掲示板に投稿する</h3>
+    <label for="name">ニックネーム：</label>
+    <input
+      id="name"
+      type="text"
+      v-model="name"
+    >
+    <br><br>
+    <label for="comment">コメント：</label>
+    <textarea id="comment" v-model="comment"></textarea>
+    <br><br>
+    <button @click="createComment">コメントをサーバーに送る</button>
+    <h2>掲示板</h2>
+    <div
+      v-for="post in posts"
+      :key="post.name"
+    >
+      <br>
+      <div>名前：{{ post.fields.name.stringValue }}</div>
+      <div>コメント：{{ post.fields.comment.stringValue }}</div>
+    </div>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import axios from "axios";
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
+  data() {
+    return {
+      name: "",
+      comment: "",
+      posts: []
+    };
+  },
+  created() {
+    axios
+      .get("/comments",
+      )
+      .then(response => {
+        this.posts = response.data.documents;
+      });
+  },
+  methods: {
+    createComment() {
+      axios
+        .post("/comments", {
+            fields: {
+              name: {
+                stringValue: this.name
+              },
+              comment: {
+                stringValue: this.comment
+              }
+            }
+          }
+        );
+      this.name = "";
+      this.comment = "";
+    }
   }
-}
+};
 </script>
 
 <style>
--- src/main.js
+++ src/main.js
@@ -1,4 +1,8 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import axios from "axios";
+
+axios.defaults.baseURL =
+  "https://firestore.googleapis.com/v1/projects/vuejs-section17/databases/(default)/documents";
 
 createApp(App).mount('#app')
